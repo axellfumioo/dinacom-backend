e224ec58ceed51c847c9f327601d9005cb7cb699
Firegame224 <xg474627@gmail.com>
2025-12-23 20:30:09 +0700

    refactor(types): menambahkan Type Description untuk struct AINutritionResponse

--- app/types/ai_foodscan_response.go
+++ app/types/ai_foodscan_response.go
@@ -1,9 +1,10 @@
 package types
 
 type AINutritionResponse struct {
-	FoodName      []string  `json:"food_name"`
-	Calories      int     `json:"calories"`
-	Protein       float64 `json:"protein"`
-	Carbohydrates float64 `json:"carbohydrates"`
-	Fat           float64 `json:"fat"`
+	FoodName      []string `json:"food_name"`
+	Calories      int      `json:"calories"`
+	Protein       float64  `json:"protein"`
+	Carbohydrates float64  `json:"carbohydrates"`
+	Fat           float64  `json:"fat"`
+	Descriptions  string   `json:"description"`
 }
