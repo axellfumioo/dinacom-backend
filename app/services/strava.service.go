0be935c77e9e1eba22afa07c69d1b76018967fba
Firegame224 <xg474627@gmail.com>
2025-12-29 17:55:27 +0700

    feat(strava): menambahkan response type untuk GetStravaProfile untuk type safety

--- app/services/strava.service.go
+++ app/services/strava.service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"backend-dinakom/app/helpers"
+	"backend-dinakom/app/types"
 	"errors"
 
 	"github.com/go-resty/resty/v2"
@@ -9,7 +10,7 @@ import (
 )
 
 type StravaService interface {
-	GetStravaProfile(UserID string) (any, error)
+	GetStravaProfile(UserID string) (*types.StravaProfileResponse, error)
 }
 
 type stravaService struct {
@@ -21,16 +22,28 @@ func NewStravaService(db *gorm.DB, restyClient *resty.Client) StravaService {
 	return &stravaService{db: db, restyClient: restyClient}
 }
 
-func (s *stravaService) GetStravaProfile(UserID string) (any, error) {
+func (s *stravaService) GetStravaProfile(UserID string) (*types.StravaProfileResponse, error) {
 	stravaAccessToken, err := helpers.GetValidStravaToken(s.db, UserID)
 	if err != nil {
 		return nil, err
 	}
 
-	var result any
+	var result types.StravaProfileResponse
 	if _, err := s.restyClient.R().SetAuthToken(*stravaAccessToken).SetResult(&result).Get("https://www.strava.com/api/v3/athlete"); err != nil {
 		return nil, errors.New("failed to get strava profile")
 	}
 
-	return &result, nil
+	stravaResponse := &types.StravaProfileResponse{
+		ID:            result.ID,
+		Username:      result.Username,
+		Firstname:     result.Firstname,
+		Lastname:      result.Lastname,
+		Profile:       result.Profile,
+		ProfileMedium: result.ProfileMedium,
+		State:         result.State,
+		ResourceState: result.ResourceState,
+		Sex:           result.Sex,
+		CreatedAt:     result.CreatedAt,
+	}
+	return stravaResponse, nil
 }
--- app/types/strava_response.go
+++ app/types/strava_response.go
@@ -1,8 +1,26 @@
 package types
 
+import "time"
+
 type StravaTokenResponse struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	ExpiresAt    int64  `json:"expires_at"`
 	Athlete      any    `json:"athlete"`
+}
+
+type StravaProfileResponse struct {
+	ID            int64     `json:"id"`
+	Firstname     string    `json:"firstname"`
+	Lastname      string    `json:"lastname"`
+	Premium       bool      `json:"premium"`
+	Profile       string    `json:"profile"`
+	ProfileMedium string    `json:"profile_medium"`
+	ResourceState int       `json:"resource_state"`
+	Sex           string    `json:"sex"`
+	State         string    `json:"state"`
+	Summit        bool      `json:"summit"`
+	Username      *string   `json:"username"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time   `json:"updated_at"`
 }
