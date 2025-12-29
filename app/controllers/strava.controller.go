d2b3ca81a446bde28506cd560c19a1bb99a09f4d
Firegame224 <xg474627@gmail.com>
2025-12-29 20:11:05 +0700

    docs(strava): menambahkan dokumentasi untuk strava route

--- app/controllers/strava.controller.go
+++ app/controllers/strava.controller.go
@@ -22,6 +22,16 @@ func NewStravaController(stravaService services.StravaService) StravaController 
 	return &stravaController{stravaService: stravaService}
 }
 
+// GetStravaProfile godoc
+// @Summary GetStravaProfile
+// @Description Access this endpoint to get user strava profile
+// @Tags Strava
+// @Produce  json
+// @Success 200 {object} response.SuccessResponse
+// @Failure 400 {object} response.ErrorResponse
+// @Failure 404 {object} response.ErrorResponse
+// @Router /strava/profile [get]
+// @Security BearerAuth
 func (ctrl *stravaController) GetStravaProfile(c *fiber.Ctx) error {
 	UserID := c.Locals("user_id").(string)
 
@@ -33,6 +43,18 @@ func (ctrl *stravaController) GetStravaProfile(c *fiber.Ctx) error {
 	return helpers.SuccessResponse(c, "strava profile retrieved successfully", data)
 }
 
+// GetStravaActivities godoc
+// @Summary GetStravaActivities
+// @Description Access this endpoint to get user strava activities
+// @Tags Strava
+// @Produce  json
+// @Param page query int false "Page number" default(1)
+// @Param page_size query int false "Page size" default(10) minimum(1) maximum(100)
+// @Success 200 {object} response.SuccessResponse
+// @Failure 400 {object} response.ErrorResponse
+// @Failure 404 {object} response.ErrorResponse
+// @Router /strava/activities [get]
+// @Security BearerAuth
 func (ctrl *stravaController) GetStravaActivities(c *fiber.Ctx) error {
 	UserID := c.Locals("user_id").(string)
 	page, _ := strconv.Atoi(c.Query("page", "1"))
@@ -46,6 +68,17 @@ func (ctrl *stravaController) GetStravaActivities(c *fiber.Ctx) error {
 	return helpers.SuccessResponse(c, "strava activities retrieved successfully", data)
 }
 
+// GetStravaActivityByID godoc
+// @Summary GetStravaActivityByID
+// @Description Access this endpoint to get user strava activity by ID
+// @Tags Strava
+// @Produce  json
+// @Param id path string true "ID"
+// @Success 200 {object} response.SuccessResponse
+// @Failure 400 {object} response.ErrorResponse
+// @Failure 404 {object} response.ErrorResponse
+// @Router /strava/activities/{id} [get]
+// @Security BearerAuth
 func (ctrl *stravaController) GetStravaActivityByID(c *fiber.Ctx) error {
 	UserID := c.Locals("user_id").(string)
 	ID, _ := strconv.Atoi(c.Params("id"))
