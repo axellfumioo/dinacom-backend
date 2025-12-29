ac1bf07a75421eb3224e3ba897100c17bd86fe7b
Firegame224 <xg474627@gmail.com>
2025-12-29 17:44:07 +0700

    feat(strava): menambahkan handler untuk mendapatkan data akun strava milik user

--- app/controllers/strava.controller.go
+++ app/controllers/strava.controller.go
@@ -1,8 +1,15 @@
 package controllers
 
-import "backend-dinakom/app/services"
+import (
+	"backend-dinakom/app/helpers"
+	"backend-dinakom/app/services"
 
-type StravaController interface{}
+	"github.com/gofiber/fiber/v2"
+)
+
+type StravaController interface {
+	GetStravaProfile(c *fiber.Ctx) error
+}
 
 type stravaController struct {
 	stravaService services.StravaService
@@ -10,4 +17,15 @@ type stravaController struct {
 
 func NewStravaController(stravaService services.StravaService) StravaController {
 	return &stravaController{stravaService: stravaService}
+}
+
+func (ctrl *stravaController) GetStravaProfile(c *fiber.Ctx) error {
+	UserID := c.Locals("user_id").(string)
+
+	data, err := ctrl.stravaService.GetStravaProfile(UserID)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return helpers.SuccessResponse(c, "strava profile retrieved successfully", data)
 }
--- app/services/strava.service.go
+++ app/services/strava.service.go
@@ -1,11 +1,16 @@
 package services
 
 import (
+	"backend-dinakom/app/helpers"
+	"errors"
+
 	"github.com/go-resty/resty/v2"
 	"gorm.io/gorm"
 )
 
-type StravaService interface{}
+type StravaService interface {
+	GetStravaProfile(UserID string) (any, error)
+}
 
 type stravaService struct {
 	db          *gorm.DB
@@ -14,4 +19,18 @@ type stravaService struct {
 
 func NewStravaService(db *gorm.DB, restyClient *resty.Client) StravaService {
 	return &stravaService{db: db, restyClient: restyClient}
+}
+
+func (s *stravaService) GetStravaProfile(UserID string) (any, error) {
+	stravaAccessToken, err := helpers.GetValidStravaToken(s.db, UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	var result any
+	if _, err := s.restyClient.R().SetAuthToken(*stravaAccessToken).SetResult(&result).Get("https://www.strava.com/api/v3/athlete"); err != nil {
+		return nil, errors.New("failed to get strava profile")
+	}
+
+	return &result, nil
 }
