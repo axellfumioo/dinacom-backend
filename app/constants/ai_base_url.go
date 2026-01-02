587f00adfe540cc14a6e911df92b83f3dc42b083
Firegame224 <xg474627@gmail.com>
2026-01-02 11:36:33 +0700

    feat: menambahkan ai api base url di config

---
+++ app/constants/ai_base_url.go
@@ -0,0 +1,5 @@
+package constants
+
+import "backend-dinakom/configs"
+
+var AiAPIBaseUrl = configs.AppConfig.App.AI_BACKEND_URL
--- configs/app.go
+++ configs/app.go
@@ -18,10 +18,11 @@ type Config struct {
 }
 
 type AppConfigType struct {
-	Name         string
-	Env          string
-	Port         string
-	Frontend_URL string
+	Name           string
+	Env            string
+	Port           string
+	Frontend_URL   string
+	AI_BACKEND_URL string
 }
 
 type DatabaseConfig struct {
@@ -72,10 +73,11 @@ func LoadConfig() {
 
 	AppConfig = &Config{
 		App: AppConfigType{
-			Name:         GetEnv("APP_NAME", "Backend-Dinacom"),
-			Env:          GetEnv("APP_ENV", "development"),
-			Port:         GetEnv("APP_PORT", "8080"),
-			Frontend_URL: GetEnv("FRONTEND_BASE_URL", "http://localhost:3000"),
+			Name:           GetEnv("APP_NAME", "Backend-Dinacom"),
+			Env:            GetEnv("APP_ENV", "development"),
+			Port:           GetEnv("APP_PORT", "8080"),
+			Frontend_URL:   GetEnv("FRONTEND_BASE_URL", "http://localhost:3000"),
+			AI_BACKEND_URL: GetEnv("AI_BACKEND_BASE_URL", "http://localhost:8000/api/v1"),
 		},
 		Database: DatabaseConfig{
 			Host:     GetEnv("DB_HOST", "localhost"),
