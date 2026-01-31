fba9bc8f660443f2c2844575c513ce9e0c98374a
Firegame224 <xg474627@gmail.com>
2026-01-31 14:13:23 +0700

    feat(notifications): menambahkan fonnte service untuk broadcasting whatsapp

---
+++ external/services/fonnte.service.go
@@ -0,0 +1,36 @@
+package extservices
+
+import (
+	"backend-dinakom/configs"
+	"fmt"
+)
+
+func FetchFonnteAPI(userName string, phoneNumber string) error {
+	restyClient := configs.RestyClient
+	var message = fmt.Sprintf("%s, Jangan sampe kelupaan!! kamu belum scan makanan hari ini😉", userName)
+
+	var result any
+	resp, err := restyClient.R().
+	SetHeader("Content-Type", "application/x-www-form-urlencoded").
+		SetAuthToken("suKrp5PHdeGjNev54CCa").
+		SetFormData(map[string]string{
+			"target":  phoneNumber,
+			"message": message,
+		}).
+		SetResult(&result).
+		Post("https://api.fonnte.com/send")
+
+	if err != nil {
+		return err
+	}
+
+	if resp.IsError() {
+		return fmt.Errorf(fmt.Sprintf(
+			"failed send WA: status=%d body=%s",
+			resp.StatusCode(),
+			resp.String(),
+		))
+	}
+
+	return nil
+}
